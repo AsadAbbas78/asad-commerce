cc889ed214e73b8ce55e1c883559dd3f4ba1db50
AsadAbbas78 <assadgamerchannel1@gmail.com>
2024-06-10 10:02:06 +0500

    changes

---
+++ src/components/ui/AvatarDisplay.tsx
@@ -0,0 +1,62 @@
+import {
+    Github,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    User
+} from "lucide-react"
+
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types";
+import Link from "next/link"
+
+export function DropdownMenuDemo({UserData}:{UserData:KindeUser}) {
+    
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>{UserData.given_name}{" "}{UserData.family_name}</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Mail className="mr-2 h-4 w-4" />
+                    <span>{UserData.email}</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Github className="mr-2 h-4 w-4" />
+                    <Link target="_blank" href={"https://github.com/AsadAbbas78"}>GitHub</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <LifeBuoy className="mr-2 h-4 w-4" />
+                    <Link target="_blank" href={""}>Support</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <LogoutLink>Log out</LogoutLink>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+};  
---
+++ src/components/views/LandingPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+
+       <div className='flex flex-col text-center md:text-start gap-6 md:gap-0 md:flex-row justify-between items-center  container py-10 xl;py-14'>
+        <div className=' space-y-9'>
+        <p className='text-sm text-gray'>--Clothing</p>
+        </div>
+       </div>
+
+    </div>
+  )
+}
+
+export default LandingPage
